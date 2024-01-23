6533d927b2f0d87e01068eb1ea067cfecea064c1
EpicMarkH <mh403523@ohio.edu>
2024-01-23 12:32:18 -0500

    ok

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Mark Hepper
+ *        Date: 1/232024
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "HELLO GIT!" << endl;
+    return 0;
+}// main
